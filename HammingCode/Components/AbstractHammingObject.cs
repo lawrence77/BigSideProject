b244e2c60a61080d634a2093a86722e4b07a8182
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2018-05-17 23:00:16 -0400

    Add project files.

---
+++ HammingCode/Components/AbstractHammingObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HammingCode.Components
+{
+    public abstract class AbstractHammingObject 
+    {
+        public abstract HammingReport Report { get; }
+        
+        public abstract void StimulateError();
+    }
+}
---
+++ HammingCode/Components/HammingReport.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HammingCode.Components
+{
+    public class HammingReport
+    {
+        
+    }
+}
