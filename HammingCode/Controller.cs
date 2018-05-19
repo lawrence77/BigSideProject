0f167872efd2530255dfddfe7b9cbb3b5d54f6d6
Lawrence Peczkowski-Soto <lawpeczkowski@gmail.com>
2018-05-19 11:02:52 -0400

    Edited the Directory of the project.

--- HammingCode/Controller.cs
+++ HammingCode/Controller.cs
@@ -13,7 +13,8 @@ namespace HammingCode
             byte[] bytesArray = { 0x90, 0x01, 0x0a, 0x20 };
             code.ConvertByteArrayToHammingCode(bytesArray);
 
-            byte[] data = code.RetrieveValue() as byte[];
+            //byte[] data = code.RetrieveValue() as byte[];
+            
             //code.SimulateRandomError();
 
             //code.BuildReport();
